9837268f90aa280dd1a738e329b0a2e25c1b1b39
Wesley Hu <91095436+weswes2EPYC@users.noreply.github.com>
2024-07-16 07:07:59 -0400

    added js files

---
+++ js/youtubePopup.js
@@ -0,0 +1,16 @@
+const YTPlayerOverlay = document.querySelector(".youtube-player-overlay");
+const YTLinks = document.querySelectorAll(".youtube-link");
+const YTPlayerPopup = document.querySelector(".youtube-player-popup iframe");
+
+YTLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        YTPlayerOverlay.classList.add("active");
+        let videoLink = `https://www.youtube.com/embed/${link.dataset.link}`
+        YTPlayerPopup.src = videoLink;
+    });
+});
+
+YTPlayerOverlay.addEventListener("click", () => {
+    YTPlayerOverlay.classList.remove("active");
+    YTPlayerPopup.src = '';
+});
